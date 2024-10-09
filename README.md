925a00511bef0bc086b02a3bc37060dde9f38404
laxmikumari615 <laxmi87892@gmail.com>
2024-10-09 11:35:47 +0530

    main

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Set-Delivery-Optimization-Bandwidth
+
+Delivery Optimization is designed to automatically adjust the amount of network bandwidth it uses to upload and download updates.
---
+++ main.ps1
@@ -0,0 +1,27 @@
+﻿#Define Registry paths, key(s) and variables
+#############################################
+# path
+$regPathAU = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization'
+#############################################
+
+# Test AU path, if path exists then create keys, else create path then create keys
+# Bandwidth in Hexadecimal KiloBYTES (64 = Decimal 100KB = .8Mbps)
+# I doubled the value for the Foreground speed
+if (Test-Path -Path $regPathAU) {
+    'Name,Value,Type
+    DOMaxBackgroundDownloadBandwidth,510,DWORD' | 
+        ConvertFrom-Csv |
+        Set-ItemProperty -Path $regPathAU -Name { $_.Name } 
+      $response = 'AU Registry key created.'
+      Write-Output $response
+    } else {
+        New-Item -Path $regPathAU -Force
+            $response = 'Created AU registry path.'
+            Write-Output $response
+        'Name,Value,Type
+        DOMaxBackgroundDownloadBandwidth,510,DWORD' | 
+            ConvertFrom-Csv |
+            Set-ItemProperty -Path $regPathAU -Name { $_.Name } 
+        $response = 'AU Registry key created.'
+        Write-Output $response
+    }
